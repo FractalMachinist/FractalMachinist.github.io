880296680c764d83e067766ef299681e93dc4f8a
Zach Allen <fractalmachinist@gmail.com>
2022-09-06 15:31:35 -0400

    Partial Progress

--- html_to_pdf.sh
+++ html_to_pdf.sh
@@ -1,12 +1,17 @@
-mkdir docs/pdf_sources;
+mkdir -p docs/pdf_sources;
 cd docs/pdf_sources;
 
-for job_name in *
+for job_path in */to_pdf.html;
 do
-	url="http://alex:32180/pdf_sources/$job_name/to_pdf.html";
+	job_name=$(dirname "$job_path");
+	url="http://alex:32180/pdf_sources/$job_path";
 	pdf_target="$job_name/Zach_Allen_Resume.pdf";
 	echo "Converting '$url' to pdf @'$pdf_target'";
-	google-chrome --headless --run-all-compositor-stages-before-draw --print-to-pdf="$pdf_target" "$url" && echo -e "Written\n";# && sleep 1;
-	# sleep 1;
-	# exit 1;
+	google-chrome --headless --run-all-compositor-stages-before-draw --print-to-pdf="$pdf_target" "$url" && echo -e "Written";
+	echo "";
 done
+
+
+echo "Converting 'http://alex:32180/pdf_sources/index.html' to pdf @'index.pdf'";
+google-chrome --headless --run-all-compositor-stages-before-draw --print-to-pdf="index.pdf" "http://alex:32180/pdf_sources/index.html" && echo -e "Written";
+echo "";
